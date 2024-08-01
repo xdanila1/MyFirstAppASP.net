24e0373b800621d9a7262cb06254eae3d333c32f
xdanila1 <one.kekc@gmail.com>
2024-08-01 21:09:12 +0300

    День 1. Знакомство с ответом(Response), запросом(Request) и с заголовками HTTP.

--- MyFirstAppASP.net/Program.cs
+++ MyFirstAppASP.net/Program.cs
@@ -1,6 +1,27 @@
+using System.Diagnostics.CodeAnalysis;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+app.Run(async (context) =>
+{
+    var path = context.Request.Path;
+    var response = context.Response;
+    
+    if(path == "/date")
+    {
+        await response.WriteAsync($"Date: {DateTime.Now.ToShortDateString()}");
+    }
+    else if (path =="/time")
+    {
+        await response.WriteAsync($"Time: {DateTime.Now.ToShortTimeString()}");
+    }
+    else
+    {
+        await response.WriteAsync("Welcome to my app");
+    }
+
+
+});
 app.Run();
